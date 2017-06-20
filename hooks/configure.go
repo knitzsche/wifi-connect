9da7642aae0e56438de92493979ed2dd2f53d596
Kyle Nitzsche <kyle.nitzsche@canonical.com>
2017-06-20 22:20:50 +0000

    configure hook creates $SNAP_COMMON/pre-config.json
    Created from relevant snapd vars for wifi-connect

--- hooks/configure.go
+++ hooks/configure.go
@@ -6,9 +6,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 
 	"launchpad.net/wifi-connect/daemon"
@@ -27,67 +25,43 @@ func main() {
 	preConfig := &daemon.PreConfig{}
 	var val string
 	var err error
-	val, err = snapGet("passphrase")
+	val, err = snapGet("wifi.security-passphrase")
 	if err != nil {
 		log.Print("== wifi-connect/configure error", err)
-		return
 	}
 	if len(val) > 0 {
 		preConfig.Passphrase = val
 	}
 
-	val, err = snapGet("ssid")
+	val, err = snapGet("portal.password")
 	if err != nil {
 		log.Print("== wifi-connect/configure error", err)
-		return
-	}
-	if len(val) > 0 {
-		preConfig.Ssid = val
-	}
-
-	val, err = snapGet("interface")
-	if err != nil {
-		log.Print("== wifi-connect/configure error", err)
-		return
-	}
-	if len(val) > 0 {
-		preConfig.Interface = val
-	}
-
-	val, err = snapGet("password")
-	if err != nil {
-		log.Print("== wifi-connect/configure error", err)
-		return
 	}
 	if len(val) > 0 {
 		preConfig.Password = val
 	}
 
-	val, err = snapGet("no-operational")
+	val, err = snapGet("portal.no-operational")
 	if err != nil {
 		log.Print("== wifi-connect/configure error", err)
-		return
 	}
 	preConfig.NoOperational = false // default
 	if val == "true" {
 		preConfig.NoOperational = true
 	}
 
-	val, err = snapGet("no-reset-creds")
+	val, err = snapGet("portal.no-reset-creds")
 	if err != nil {
 		log.Print("== wifi-connect/configure error", err)
-		return
 	}
 	preConfig.NoResetCreds = false // default
 	if val == "true" {
 		preConfig.NoResetCreds = true
 	}
 
-	confFile := filepath.Join(os.Getenv("SNAP_COMMON"), "pre-config.json")
-
 	b, errJM := json.Marshal(preConfig)
 	if errJM == nil {
-		errWJ := ioutil.WriteFile(confFile, b, 0644)
+		errWJ := ioutil.WriteFile(daemon.PreConfigFile, b, 0644)
 		if errWJ != nil {
 			log.Print("== wifi-connect/configure error:", errWJ)
 			return
